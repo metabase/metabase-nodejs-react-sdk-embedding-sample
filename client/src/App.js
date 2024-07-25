7a183360f94f6454f3753758b3a0fb8ec6a2191c
Alberto Perdomo <alberto.perdomo@gmail.com>
2024-07-25 12:14:54 +0100

    SDK embedding working

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import axios from 'axios';
 import './App.css';
+import { MetabaseProvider, InteractiveQuestion } from "@metabase/embedding-sdk-react";
 
-//data will be the string we send from our server
-const apiCall = () => {
-  axios.get('http://localhost:9090').then((data) => {
-    //this console.log will be in our frontend console
-    console.log(data)
-  })
+// Configuration
+const config = {
+  metabaseInstanceUrl: process.env.REACT_APP_METABASE_INSTANCE_URL,
+  jwtProviderUri: process.env.REACT_APP_JWT_PROVIDER_URI
+}
+
+const questionId = 14;
+
+const theme = {
+  // Specify a font to use from the set of fonts supported by Metabase.
+  // You can set the font to "Custom" to use the custom font
+  // configured in your Metabase instance.
+  fontFamily: "Lato",
+
+  // Override the base font size for every component.
+  // This does not usually need to be set, as the components
+  // inherit the font size from the parent container, such as the body.
+  fontSize: "16px",
+
+  // Override the base line height for every component.
+  lineHeight: 1.5,
+
+  // Match your application's color scheme
+  colors: {
+    brand: "#d21238",
+    "text-primary": "#441616",
+    "text-secondary": "#8d1b01",
+    "text-tertiary": "#1d78f0",
+    background: "#f0dbdb",
+    "background-hover": "#e2e1e1",
+    border: "#3e0606",
+    filter: "#b54504",
+    summarize: "#3c680d",
+    shadow: "rgba(0,0,0,0.08)",
+  },
+
+  table: {
+    cell: {
+      textColor: "#4C5773",
+      backgroundColor: "#FFFFFF",
+    },
+
+    idColumn: {
+      textColor: "#9B5966",
+      backgroundColor: "#F5E9EB",
+    },
+  },
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" width="1200px" height="800px">
+      <MetabaseProvider config={config} theme={theme}>
+        <InteractiveQuestion questionId={questionId} />
+      </MetabaseProvider>
 
-        <button onClick={apiCall}>Make API Call</button>
-      </header>
     </div>
   );
+
 }
 
 export default App;
