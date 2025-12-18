88d7ee0a1ab6425b1b1be8c3da37eeff934bdb1f
Rodrigo López Dato <rodrigo@metabase.com>
2025-12-18 16:41:41 -0300

    prettier/eslint

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import eslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig(
   { ignores: ["dist"] },
@@ -18,8 +18,8 @@ export default defineConfig(
   {
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
   },
-)
+);
